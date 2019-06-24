91b556341ac988843a6a2af01d55582ac7f28c79
Prasith Prabhu <prasithtoughboy@gmail.com>
2019-06-24 17:22:42 +0530

    Removed the back path in json path variable

--- gke/gcp.tf
+++ gke/gcp.tf
@@ -2,7 +2,7 @@
 # Google Cloud Platform
 #####################################################################
 provider "google" {
-  credentials = "${file("../gcloud-sa.json")}"
+  credentials = "${file("gcloud-sa.json")}"
   project = "${var.project}"
   region  = "${var.region}"
 }
