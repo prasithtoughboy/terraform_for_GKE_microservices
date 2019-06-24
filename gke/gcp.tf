d861da1216ba41a1caf6c5c71469290409200c39
Prasith Prabhu <prasithtoughboy@gmail.com>
2019-06-24 17:16:45 +0530

    Changed the credential path

--- gke/gcp.tf
+++ gke/gcp.tf
@@ -2,7 +2,7 @@
 # Google Cloud Platform
 #####################################################################
 provider "google" {
-  credentials = "${file("/home/vvdn/Downloads/gcloud-sa.json")}"
+  credentials = "${file("../gcloud-sa.json")}"
   project = "${var.project}"
   region  = "${var.region}"
 }
